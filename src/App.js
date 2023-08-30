26fabb57670d2a2e69f784baf062efbcff98a0c6
Gabriel Alpha Aguilar <gabrielalpha98@gmail.com>
2023-08-30 09:26:44 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Blob } from './components/spline-landing';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{position: 'absolute', width: 560, right: 96}}>
+          <h1 style={{color: "#F9FF99"}}>Spline Test</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ante libero, eleifend non urna vel, lacinia vehicula ante. In erat lectus, volutpat vel placerat quis, tincidunt ut elit. Integer lorem velit, egestas nec lacus a, porta laoreet nulla. Pellentesque tortor tortor, congue eget ultricies ac, ultricies gravida sem.</p>
+        </div>
+        <Blob />
       </header>
     </div>
   );
---
+++ src/components/spline-landing.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+const Blob = () => {
+  return (
+    <Spline scene="https://prod.spline.design/uSPBnhsCc9RBcg8c/scene.splinecode" />
+  );
+}
+
+export {Blob}
